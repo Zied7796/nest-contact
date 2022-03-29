7750eee40adb9c24ee10b21a3af9b18b2b11225f
Zied7796 <zhamdi8@gmail.com>
2022-03-29 18:38:20 +0200

    app_controller and service

--- contact-nest/src/contact/app.controller.ts
+++ contact-nest/src/contact/app.controller.ts
@@ -1,25 +1,18 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 import CreateUserDto from './dto/create-user.dto';
+import UpdateUserDto from './dto/update-user.dto';
 import { User } from './schemas/user.schema';
 
-// @Controller()
-// export class AppController {
-//   constructor(private readonly appService: AppService) {}
-
-//   @Get()
-//   getHello(): string {
-//     return this.appService.getHello();
-//   }
-// }
 @Controller('user')
-export class UserController {
+export class AppController {
+  userService: any;
   constructor(private readonly appService: AppService) {}
 
   @Get()
   
   findAll() {
-    return this.appService.findAll();
+    return this.appService.getHello();
   }
 
   @Post()
@@ -32,7 +25,7 @@ export class UserController {
     @Param('id') id: string,
     @Body() updateUserDto: UpdateUserDto,
   ): Promise<User> {
-    return this.userService.update(id, updateUserDto);
+    return this.appService.update(id, updateUserDto);
   }
 
   @Delete(':id')
