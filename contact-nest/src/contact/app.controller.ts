3265657d8799b6fcb90db30c7a766cbe54ad2946
Zied7796 <zhamdi8@gmail.com>
2022-03-29 18:37:32 +0200

    formated_code

--- contact-nest/src/contact/app.controller.ts
+++ contact-nest/src/contact/app.controller.ts
@@ -1,12 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import CreateUserDto from './dto/create-user.dto';
+import { User } from './schemas/user.schema';
 
-@Controller()
-export class AppController {
+// @Controller()
+// export class AppController {
+//   constructor(private readonly appService: AppService) {}
+
+//   @Get()
+//   getHello(): string {
+//     return this.appService.getHello();
+//   }
+// }
+@Controller('user')
+export class UserController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  
+  findAll() {
+    return this.appService.findAll();
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.appService.create(createUserDto);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    return this.userService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.userService.delete(id);
   }
 }
